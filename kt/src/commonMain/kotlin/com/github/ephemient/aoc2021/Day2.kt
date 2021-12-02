261826ee3f31b76942f86b6b4c31b580a01a1371
Daniel Lin <ephemient@gmail.com>
2021-12-02 01:56:08 -0500

    Day 2: split is faster than regex match

--- kt/src/commonMain/kotlin/com/github/ephemient/aoc2021/Day2.kt
+++ kt/src/commonMain/kotlin/com/github/ephemient/aoc2021/Day2.kt
@@ -6,11 +6,12 @@ class Day2(private val lines: List<String>) {
         var x = 0
         var y = 0
         for (line in lines) {
-            val m = requireNotNull(PATTERN.matchEntire(line)) { line }
-            m.groups[1]?.value?.let { x += it.toInt() }
-                ?: m.groups[2]?.value?.let { y += it.toInt() }
-                ?: m.groups[3]?.value?.let { y -= it.toInt() }
-                ?: throw IllegalArgumentException(line)
+            val (prefix, suffix) = line.split(" ", limit = 2)
+            when (prefix) {
+                "forward" -> x += suffix.toInt()
+                "down" -> y += suffix.toInt()
+                "up" -> y -= suffix.toInt()
+            }
         }
         return x * y
     }
@@ -20,20 +21,16 @@ class Day2(private val lines: List<String>) {
         var y = 0
         var depth = 0
         for (line in lines) {
-            val m = requireNotNull(PATTERN.matchEntire(line)) { line }
-            m.groups[1]?.value?.let {
-                val d = it.toInt()
-                x += d
-                y += d * depth
+            val (prefix, suffix) = line.split(" ", limit = 2)
+            when (prefix) {
+                "forward" -> suffix.toInt().let {
+                    x += it
+                    y += it * depth
+                }
+                "down" -> depth += suffix.toInt()
+                "up" -> depth -= suffix.toInt()
             }
-                ?: m.groups[2]?.value?.let { depth += it.toInt() }
-                ?: m.groups[3]?.value?.let { depth -= it.toInt() }
-                ?: throw IllegalArgumentException(line)
         }
         return x * y
-    }
-
-    companion object {
-        private val PATTERN = """forward (\d+)|down (\d+)|up (\d+)""".toRegex()
     }
 }
