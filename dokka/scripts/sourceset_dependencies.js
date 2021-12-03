0af196ab450a249619861bbc78d9bce84c4325b9
ephemient <ephemient@users.noreply.github.com>
2021-12-03 06:20:45 +0000

    Dokka 0478c41998f49ecb25869cb3dd31f6fa920620b4

--- dokka/scripts/sourceset_dependencies.js
+++ dokka/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies='{":dokkaHtml/commonMain":[],":dokkaHtml/jvmMain":[":dokkaHtml/commonMain"]}'
+sourceset_dependencies='{":dokkaHtml/commonMain":[],":dokkaHtml/jsMain":[":dokkaHtml/commonMain",":dokkaHtml/nonJvmMain"],":dokkaHtml/jvmMain":[":dokkaHtml/commonMain"],":dokkaHtml/linuxX64Main":[":dokkaHtml/commonMain",":dokkaHtml/nonJvmMain"],":dokkaHtml/nonJvmMain":[]}'
