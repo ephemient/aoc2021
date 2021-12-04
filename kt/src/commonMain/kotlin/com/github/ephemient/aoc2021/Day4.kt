376779e01bffac3ec8410d8c94e37d4b6547c520
Daniel Lin <ephemient@gmail.com>
2021-12-04 08:16:26 -0500

    Day 4: Change representation for performance

--- kt/src/commonMain/kotlin/com/github/ephemient/aoc2021/Day4.kt
+++ kt/src/commonMain/kotlin/com/github/ephemient/aoc2021/Day4.kt
@@ -1,8 +1,5 @@
 package com.github.ephemient.aoc2021
 
-private typealias BingoBoard = List<List<Int?>>
-private typealias MutableBingoBoard = MutableList<MutableList<Int?>>
-
 class Day4(lines: List<String>) {
     private val draws: List<Int>
     private val boards: List<BingoBoard>
@@ -10,57 +7,90 @@ class Day4(lines: List<String>) {
         val iterator = lines.iterator()
         check(iterator.hasNext())
         draws = iterator.next().split(',').map { it.toInt() }
-        check(iterator.hasNext() && iterator.next().isEmpty())
         boards = buildList {
+            if (!iterator.hasNext()) return@buildList
+            check(iterator.next().isEmpty())
             while (iterator.hasNext()) {
-                add(
-                    buildList {
-                        for (line in iterator) {
-                            if (line.isEmpty()) break
-                            add(line.split(WHITESPACE).filter { it.isNotEmpty() }.map { it.toIntOrNull() })
-                        }
+                var width = -1
+                var height = 0
+                val unmarked = mutableMapOf<Int, Int>()
+                while (iterator.hasNext()) {
+                    val line = iterator.next()
+                    if (line.isEmpty()) break
+                    val row = line.split(WHITESPACE).filter { it.isNotEmpty() }.map { it.toInt() }
+                    check(row.isNotEmpty())
+                    if (height++ == 0) {
+                        width = row.size
+                    } else {
+                        check(width == row.size)
                     }
-                )
+                    for ((x, n) in row.withIndex()) {
+                        unmarked[n] = unmarked.getOrElse(n) { 0 } or (1 shl x + width * (height - 1))
+                    }
+                }
+                check(height > 0)
+                add(BingoBoard(width, height, unmarked, 0))
             }
         }
     }
 
     fun part1(): Int? {
-        val boards: List<MutableBingoBoard> = boards.map { it.toMutableBoard() }
+        val boards = boards.map { it.copy() }
         for (draw in draws) {
             for (board in boards) {
-                board.mark(draw)
-                if (board.isBingo()) return draw * board.sum()
+                if (board.play(draw)) {
+                    return draw * board.sum()
+                }
             }
         }
         return null
     }
 
-    fun part2(): Int? = boards.mapNotNull {
-        val board = it.toMutableBoard()
-        for ((i, draw) in draws.withIndex()) {
-            board.mark(draw)
-            if (board.isBingo()) return@mapNotNull i to draw * board.sum()
+    fun part2(): Int? {
+        val boards = boards.mapTo(mutableListOf()) { it.copy() }
+        var score: Int? = null
+        for (draw in draws) {
+            boards.retainAll { board ->
+                if (board.play(draw)) {
+                    score = draw * board.sum()
+                    false
+                } else true
+            }
+            if (boards.isEmpty()) break
         }
-        return@mapNotNull null
-    }.maxByOrNull { it.first }?.second
+        return score
+    }
 
-    companion object {
-        private val WHITESPACE = """\s""".toRegex()
+    private class BingoBoard(
+        private val width: Int,
+        private val height: Int,
+        private val unmarked: MutableMap<Int, Int>,
+        private var marked: Int,
+    ) {
+        init {
+            require(width * height < Int.SIZE_BITS)
+        }
 
-        private fun BingoBoard.toMutableBoard(): MutableBingoBoard = mapTo(mutableListOf()) { it.toMutableList() }
-        private fun MutableBingoBoard.mark(draw: Int) {
-            for (row in this) {
-                val iterator = row.listIterator()
-                while (iterator.hasNext()) {
-                    if (iterator.next() == draw) iterator.set(null)
+        fun copy(): BingoBoard = BingoBoard(width, height, unmarked.toMutableMap(), marked)
+
+        fun play(draw: Int): Boolean {
+            val marks = unmarked.remove(draw) ?: return false
+            marked = marks or marked
+            return (0 until height).any { y ->
+                val mask = (1 shl width) - 1 shl y * width
+                marked and mask == mask
+            } || run {
+                (0 until width).any { x ->
+                    val mask = (((1u shl width * height) - 1u) / ((1u shl width) - 1u)).toInt() shl x
+                    marked and mask == mask
                 }
             }
         }
-        private fun BingoBoard.isBingo(): Boolean = any { row -> row.all { it == null } } ||
-            (0 until (maxOfOrNull { it.size } ?: 0)).any { column ->
-                all { it.getOrNull(column) == null }
-            }
-        private fun BingoBoard.sum(): Int = sumOf { row -> row.sumOf { it ?: 0 } }
+
+        fun sum(): Int = unmarked.entries.sumOf { (key, value) -> key * value.countOneBits() }
+    }
+
+    companion object {
+        private val WHITESPACE = """\s""".toRegex()
     }
 }
