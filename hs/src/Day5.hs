21d2e7eb9d50f28f7d6abe406bfeb27b89ff831e
Daniel Lin <ephemient@gmail.com>
2021-12-05 05:17:35 -0500

    Day 5: Speed up with bit manipulation

--- hs/src/Day5.hs
+++ hs/src/Day5.hs
@@ -2,40 +2,69 @@
 Module:         Day5
 Description:    <https://adventofcode.com/2021/day/5 Day 5: Hydrothermal Venture>
 -}
-{-# LANGUAGE FlexibleContexts, MultiWayIf, OverloadedStrings, TupleSections, TypeApplications, TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts, MultiWayIf, OverloadedStrings, ScopedTypeVariables, TypeFamilies #-}
 module Day5 (day5a, day5b) where
 
-import qualified Data.Map as Map (elems, fromListWith)
+import Control.Monad (when)
+import Data.Bits (Bits((.&.), (.|.), bitSizeMaybe, shiftL, shiftR, xor), finiteBitSize)
+import Data.Function (on)
+import qualified Data.IntMap as IntMap (elems, fromSet, unionsWith)
+import qualified Data.IntSet as IntSet (fromDistinctAscList)
+import Data.List.NonEmpty (nonEmpty)
+import qualified Data.Set as Set (empty)
 import Data.String (IsString)
 import Data.Text (Text)
 import Data.Void (Void)
-import Text.Megaparsec (MonadParsec, ParseErrorBundle, Token, Tokens, eof, parse, sepEndBy)
+import Text.Megaparsec (ErrorItem(Label), MonadParsec, ParseErrorBundle, Token, Tokens, eof, failure, parse, sepEndBy)
 import Text.Megaparsec.Char (char, newline, string)
 import Text.Megaparsec.Char.Lexer (decimal)
 
-parser :: (Num a, MonadParsec e s m, Token s ~ Char, IsString (Tokens s)) => m [((a, a), (a, a))]
-parser = line `sepEndBy` newline <* eof where
-    line = (,) <$> pair <* string " -> " <*> pair
-    pair = (,) <$> decimal <* char ',' <*> decimal
+parser :: forall e s m a. (MonadParsec e s m, Token s ~ Char, IsString (Tokens s), Bits a, Num a, Ord a) => Int -> m [(a, a)]
+parser bitsPerComponent
+  | bitsPerComponent <= 0 = error "out of range"
+  | Just bitSize <- bitSizeMaybe (undefined :: a)
+  , bitSize < 2 * bitsPerComponent = error "unsupported"
+  | otherwise = line `sepEndBy` newline <* eof
+  where
+    line = do
+        first <- pair <* string " -> "
+        second <- pair
+        pure (min first second, max first second)
+    pair = do
+        x <- boundedDecimal <* char ','
+        y <- boundedDecimal
+        pure $ x `shiftL` bitsPerComponent .|. y
+    boundedDecimal = do
+        value <- decimal
+        when (value `shiftR` bitsPerComponent /= 0) $
+            failure (Label <$> nonEmpty "out of range") Set.empty
+        pure value
 
 day5a :: Text -> Either (ParseErrorBundle Text Void) Int
 day5a input = do
-    segments <- parse (parser @Int) "" input
-    pure $ length $ filter (> 1) $ Map.elems $ Map.fromListWith (+) $ do
-        ((x0, y0), (x1, y1)) <- segments
-        (, 1 :: Int) <$> if
-          | x0 == x1 -> [(x0, y) | y <- [min y0 y1..max y0 y1]]
-          | y0 == y1 -> [(x, y0) | x <- [min x0 x1..max x0 x1]]
-          | otherwise -> []
+    segments <- parse (parser bitsPerComponent) "" input
+    pure $ length $ filter (> 1) $ IntMap.elems $ IntMap.unionsWith (+) $ do
+        (start, end) <- segments
+        let delta = start `xor` end
+        IntMap.fromSet (const (1 :: Int)) . IntSet.fromDistinctAscList <$> if
+          | delta `shiftR` bitsPerComponent == 0  -> pure [start..end]
+          | delta .&. (1 `shiftL` bitsPerComponent - 1) == 0 ->
+                pure [start, start + 1 `shiftL` bitsPerComponent..end]
+          | otherwise -> mempty
+  where bitsPerComponent = finiteBitSize (0 :: Int) `div` 2
 
 day5b :: Text -> Either (ParseErrorBundle Text Void) Int
 day5b input = do
-    segments <- parse (parser @Int) "" input
-    pure $ length $ filter (> 1) $ Map.elems $ Map.fromListWith (+) $ do
-        ((x0, y0), (x1, y1)) <- segments
-        (, 1 :: Int) <$> if
-          | x0 == x1 -> [(x0, y) | y <- [min y0 y1..max y0 y1]]
-          | y0 == y1 -> [(x, y0) | x <- [min x0 x1..max x0 x1]]
-          | x1 - x0 == y1 - y0 -> zip [min x0 x1..max x0 x1] [min y0 y1..max y0 y1]
-          | x1 - x0 == y0 - y1 -> zip [min x0 x1..max x0 x1] $ reverse [min y0 y1..max y0 y1]
-          | otherwise -> error $ show ((x0, y0), (x1, y1))
+    segments <- parse (parser bitsPerComponent) "" input
+    pure $ length $ filter (> 1) $ IntMap.elems $ IntMap.unionsWith (+) $ do
+        (start, end) <- segments
+        let delta = start `xor` end
+        pure $ IntMap.fromSet (const (1 :: Int)) $ IntSet.fromDistinctAscList $ if
+          | delta `shiftR` bitsPerComponent == 0  -> [start..end]
+          | delta .&. (1 `shiftL` bitsPerComponent - 1) == 0 ->
+                [start, start + 1 `shiftL` bitsPerComponent..end]
+          | ((-) `on` (`shiftR` bitsPerComponent)) start end ==
+                ((-) `on` (.&. (1 `shiftL` bitsPerComponent - 1))) start end ->
+                [start, start + 1 `shiftL` bitsPerComponent + 1..end]
+          | otherwise -> [start, start + 1 `shiftL` bitsPerComponent - 1..end]
+  where bitsPerComponent = finiteBitSize (0 :: Int) `div` 2
