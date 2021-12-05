d539555056084ea66f069e6f85bde8c390a7a7d3
Daniel Lin <ephemient@gmail.com>
2021-12-05 15:10:29 -0500

    Day 5: Simplify by ensuring order

--- kt/src/commonMain/kotlin/com/github/ephemient/aoc2021/Day5.kt
+++ kt/src/commonMain/kotlin/com/github/ephemient/aoc2021/Day5.kt
@@ -1,14 +1,12 @@
 package com.github.ephemient.aoc2021
 
-import kotlin.math.abs
-import kotlin.math.sign
-
 class Day5(lines: List<String>) {
     private val segments = lines.map { line ->
-        val (start, end) = line.split(" -> ", limit = 2)
-        val (x0, y0) = start.split(',', limit = 2)
-        val (x1, y1) = end.split(',', limit = 2)
-        Pair(x0.toInt() to y0.toInt(), x1.toInt() to y1.toInt())
+        val (first, second) = line.split(" -> ", limit = 2).map { word ->
+            val (x, y) = word.split(',', limit = 2)
+            x.toInt() to y.toInt()
+        }
+        if (compareValuesBy(first, second, { it.first }, { it.second }) < 0) first to second else second to first
     }
 
     fun part1(): Int = buildMap {
@@ -16,8 +14,8 @@ class Day5(lines: List<String>) {
             val (x0, y0) = start
             val (x1, y1) = end
             when {
-                x0 == x1 -> for (y in minOf(y0, y1)..maxOf(y0, y1)) put(x0 to y, getOrElse(x0 to y) { 0 } + 1)
-                y0 == y1 -> for (x in minOf(x0, x1)..maxOf(x0, x1)) put(x to y0, getOrElse(x to y0) { 0 } + 1)
+                x0 == x1 -> for (y in y0..y1) put(x0 to y, getOrElse(x0 to y) { 0 } + 1)
+                y0 == y1 -> for (x in x0..x1) put(x to y0, getOrElse(x to y0) { 0 } + 1)
             }
         }
     }.values.count { it > 1 }
@@ -27,15 +25,11 @@ class Day5(lines: List<String>) {
             val (x0, y0) = start
             val (x1, y1) = end
             when {
-                x0 == x1 -> for (y in minOf(y0, y1)..maxOf(y0, y1)) put(x0 to y, getOrElse(x0 to y) { 0 } + 1)
-                y0 == y1 -> for (x in minOf(x0, x1)..maxOf(x0, x1)) put(x to y0, getOrElse(x to y0) { 0 } + 1)
-                abs(x1 - x0) == abs(y1 - y0) -> {
-                    val dx = (x1 - x0).sign
-                    val dy = (y1 - y0).sign
-                    repeat(abs(x1 - x0)) {
-                        val xy = x0 + it * dx to y0 + it * dy
-                        put(xy, getOrElse(xy) { 0 } + 1)
-                    }
+                x0 == x1 -> for (y in y0..y1) put(x0 to y, getOrElse(x0 to y) { 0 } + 1)
+                y0 == y1 -> for (x in x0..x1) put(x to y0, getOrElse(x to y0) { 0 } + 1)
+                else -> repeat(x1 - x0 + 1) {
+                    val xy = x0 + it to if (y0 < y1) y0 + it else y0 - it
+                    put(xy, getOrElse(xy) { 0 } + 1)
                 }
             }
         }
