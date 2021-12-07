bccb70c979079f0a06980f9ec90cc1b8ec5f1d27
Daniel Lin <ephemient@gmail.com>
2021-12-07 12:12:35 -0500

    Day 4: Invert matching for performance

--- kt/src/commonMain/kotlin/com/github/ephemient/aoc2021/Day4.kt
+++ kt/src/commonMain/kotlin/com/github/ephemient/aoc2021/Day4.kt
@@ -1,94 +1,39 @@
 package com.github.ephemient.aoc2021
 
 class Day4(lines: List<String>) {
-    private val draws: List<Int>
-    private val boards: List<BingoBoard>
-    init {
+    private val boards = buildList {
         val iterator = lines.iterator()
         check(iterator.hasNext())
-        draws = iterator.next().split(',').map { it.toInt() }
-        boards = buildList {
-            if (!iterator.hasNext()) return@buildList
-            check(iterator.next().isEmpty())
-            while (iterator.hasNext()) {
-                var width = -1
-                var height = 0
-                val unmarked = mutableMapOf<Int, Int>()
-                while (iterator.hasNext()) {
-                    val line = iterator.next()
+        val draws = iterator.next().split(',').map { it.toInt() }
+        val drawTurns = buildMap<Int, Int> {
+            draws.forEachIndexed { turn, draw -> getOrPut(draw) { turn } }
+        }.withDefault { Int.MAX_VALUE }
+        if (!iterator.hasNext()) return@buildList
+        check(iterator.next().isEmpty())
+        while (iterator.hasNext()) {
+            var width = -1
+            val values = buildList {
+                for (line in iterator) {
                     if (line.isEmpty()) break
-                    val row = line.split(WHITESPACE).filter { it.isNotEmpty() }.map { it.toInt() }
-                    check(row.isNotEmpty())
-                    if (height++ == 0) {
-                        width = row.size
-                    } else {
-                        check(width == row.size)
-                    }
-                    for ((x, n) in row.withIndex()) {
-                        unmarked[n] = unmarked.getOrElse(n) { 0 } or (1 shl x + width * (height - 1))
-                    }
-                }
-                check(height > 0)
-                add(BingoBoard(width, height, unmarked, 0))
-            }
-        }
-    }
-
-    fun part1(): Int? {
-        val boards = boards.map { it.copy() }
-        for (draw in draws) {
-            for (board in boards) {
-                if (board.play(draw)) {
-                    return draw * board.sum()
+                    val row = line.split(WHITESPACE).mapNotNull { if (it.isEmpty()) null else it.toInt() }
+                    if (width < 0) width = row.size else check(width == row.size)
+                    addAll(row)
                 }
             }
-        }
-        return null
-    }
-
-    fun part2(): Int? {
-        val boards = boards.mapTo(mutableListOf()) { it.copy() }
-        var score: Int? = null
-        for (draw in draws) {
-            boards.retainAll { board ->
-                if (board.play(draw)) {
-                    score = draw * board.sum()
-                    false
-                } else true
-            }
-            if (boards.isEmpty()) break
-        }
-        return score
-    }
-
-    private class BingoBoard(
-        private val width: Int,
-        private val height: Int,
-        private val unmarked: MutableMap<Int, Int>,
-        private var marked: Int,
-    ) {
-        init {
-            require(width * height < Int.SIZE_BITS)
-        }
-
-        fun copy(): BingoBoard = BingoBoard(width, height, unmarked.toMutableMap(), marked)
-
-        fun play(draw: Int): Boolean {
-            val marks = unmarked.remove(draw) ?: return false
-            marked = marks or marked
-            return (0 until height).any { y ->
-                val mask = (1 shl width) - 1 shl y * width
-                marked and mask == mask
-            } || run {
-                (0 until width).any { x ->
-                    val mask = (((1u shl width * height) - 1u) / ((1u shl width) - 1u)).toInt() shl x
-                    marked and mask == mask
-                }
+            val turns = values.map { drawTurns.getValue(it) }
+            val turn = minOf(
+                (turns.indices step width).minOf { start -> (start until start + width).maxOf { turns[it] } },
+                (0 until width).minOf { start -> (start..turns.lastIndex step width).maxOf { turns[it] } },
+            )
+            if (turn < Int.MAX_VALUE) {
+                add(IndexedValue(turn, draws[turn] * values.sumOf { if (drawTurns.getValue(it) > turn) it else 0 }))
             }
         }
-
-        fun sum(): Int = unmarked.entries.sumOf { (key, value) -> key * value.countOneBits() }
     }
+
+    fun part1(): Int? = boards.minByOrNull { it.index }?.value
+
+    fun part2(): Int? = boards.maxByOrNull { it.index }?.value
 
     companion object {
         private val WHITESPACE = """\s""".toRegex()
