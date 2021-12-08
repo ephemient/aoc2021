57ea65f15868e27a682627a11747dd77a999ad39
Daniel Lin <ephemient@gmail.com>
2021-12-08 02:30:01 -0500

    Day 8: Hand-written decoder

--- hs/src/Day8.hs
+++ hs/src/Day8.hs
@@ -7,11 +7,8 @@ module Day8 (day8a, day8b) where
 
 import Control.Arrow (first)
 import Control.Monad (guard)
-import Data.Char (chr, ord)
-import Data.List (elemIndex, foldl', permutations, sort)
-import Data.Map (Map)
-import qualified Data.Map as Map ((!?), fromList)
-import Data.Maybe (listToMaybe, maybeToList)
+import qualified Data.IntMap as IntMap ((!?), fromListWith)
+import Data.List ((\\), elemIndex, foldl', partition, sort)
 import Data.Text (Text)
 import qualified Data.Text as T (breakOnEnd, length, lines, unpack, stripSuffix, words)
 
@@ -23,22 +20,21 @@ day8a input = length $ do
 day8b :: Text -> Maybe Int
 day8b input = sum <$> mapM handle (T.lines input)
 
-forms :: Map String Int
-forms = Map.fromList $ zip
-    ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]
-    [0..9]
-
 handle :: Text -> Maybe Int
 handle line
   | (Just lhs, rhs) <- first (T.stripSuffix " | ") $ T.breakOnEnd " | " line = do
     let canon = sort . T.unpack
         signals = canon <$> T.words lhs
         outputs = canon <$> T.words rhs
-    indices <- mapM (`elemIndex` signals) outputs
-    order <- listToMaybe $ do
-        derange <- permutations [0..6]
-        let mapping = map (chr . (+ ord 'a')) . sort . map ((derange !!) . subtract (ord 'a') . ord)
-            forms' = mapping <$> signals
-        maybeToList $ mapM (forms Map.!?) forms'
-    pure $ foldl' (\x y -> 10 * x + y) 0 $ (order !!) <$> indices
+        counts = IntMap.fromListWith (<>) [(length s, [s]) | s <- signals]
+    [one] <- counts IntMap.!? 2
+    [seven] <- counts IntMap.!? 3
+    [four] <- counts IntMap.!? 4
+    ([two], threeFive) <- partition ((== 3) . length . (\\ four)) <$> counts IntMap.!? 5
+    ([three], [five]) <- pure $ partition ((== 1) . length . (\\ two)) threeFive
+    ([six], zeroNine) <- partition (not . null . (\\) one) <$> counts IntMap.!? 6
+    ([zero], [nine]) <- pure $ partition ((== 2) . length . (\\ three)) zeroNine
+    [eight] <- counts IntMap.!? 7
+    let digits = [zero, one, two, three, four, five, six, seven, eight, nine]
+    foldl' (\x y -> 10 * x + y) 0 <$> mapM (`elemIndex` digits) outputs
   | otherwise = Nothing
