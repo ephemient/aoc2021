85c0142599736afd04b757f795ffc99a80cc0508
Daniel Lin <ephemient@gmail.com>
2021-12-09 05:51:13 -0500

    Day 9: Optimization

--- kt/src/commonMain/kotlin/com/github/ephemient/aoc2021/Day9.kt
+++ kt/src/commonMain/kotlin/com/github/ephemient/aoc2021/Day9.kt
@@ -16,21 +16,29 @@ class Day9(private val lines: List<String>) {
 
     fun part2(): Int {
         val basins = mutableListOf<Int>()
-        val visited = Array(lines.size) { BooleanArray(lines[it].length) }
+        val visited = Array(lines.size) { i ->
+            BooleanArray(lines[i].length) { j ->
+                val c = lines[i][j]
+                !c.isDigit() || c.digitToInt() >= 9
+            }
+        }
         lines.forEachIndexed { i, line ->
             line.forEachIndexed inner@{ j, c ->
                 if (visited[i][j] || !c.isDigit() || c.digitToInt() >= 9) return@inner
-                val basin = mutableSetOf(i to j)
-                val queue = ArrayDeque(listOf(i to j))
+                visited[i][j] = true
+                var basin = 0
+                val stack = mutableListOf(i to j)
                 while (true) {
-                    val (i2, j2) = queue.removeFirstOrNull() ?: break
-                    visited[i2][j2] = true
+                    val (i2, j2) = stack.removeLastOrNull() ?: break
+                    basin++
                     CardinalDirection.forEach(i2, j2) { i3, j3 ->
-                        val d = lines.getOrNull(i3)?.getOrNull(j3) ?: return@forEach
-                        if (d.isDigit() && d.digitToInt() < 9 && basin.add(i3 to j3)) queue.add(i3 to j3)
+                        if (i3 in visited.indices && j3 in visited[i3].indices && !visited[i3][j3]) {
+                            visited[i3][j3] = true
+                            stack.add(i3 to j3)
+                        }
                     }
                 }
-                basins.add(basin.size)
+                basins.add(basin)
             }
         }
         basins.sort()
