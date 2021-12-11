e6e67068128a16643a407f82fb06f7a142db927b
Daniel Lin <ephemient@gmail.com>
2021-12-11 02:14:26 -0500

    Day 11: Just in case synchonization happens *at* step 100

--- hs/src/Day11.hs
+++ hs/src/Day11.hs
@@ -15,16 +15,18 @@ import Data.Vector.Unboxed (Unbox, Vector)
 import qualified Data.Vector.Unboxed as V (accum, all, findIndex, length, map, fromList)
 
 step :: (Num a, Ord a, Unbox a) => Int -> Vector a -> Maybe (Int, Vector a)
-step width = step' 0 . V.map (+ 1) where
-    step' n v
-      | Just i <- V.findIndex (> 9) v = step' (n + 1) $ V.accum f v
+step width v
+  | V.all (== 0) v = Nothing
+  | otherwise = step' 0 $ V.map (+ 1) v
+  where
+    step' n v'
+      | Just i <- V.findIndex (> 9) v' = step' (n + 1) $ V.accum f v'
           [ (x' + y' * width, x == x' && y == y')
           | let (y, x) = i `divMod` width
           , x' <- filter (inRange (0, width - 1)) [x - 1..x + 1]
-          , y' <- filter (inRange (0, V.length v `div` width - 1)) [y - 1..y + 1]
+          , y' <- filter (inRange (0, V.length v' `div` width - 1)) [y - 1..y + 1]
           ]
-      | V.all (< 0) v = Nothing
-      | otherwise = Just (n, V.map (max 0) v)
+      | otherwise = Just (n, V.map (max 0) v')
     f _ True = -1
     f a _ = if a < 0 then a else a + 1
 
@@ -35,4 +37,4 @@ day11 input = do
     let width = T.length input0
     guard $ all ((== width) . T.length) inputs
     let flashes = unfoldr (step width) $ V.fromList $ digitToInt <$> concatMap T.unpack input'
-    pure (sum $ take 100 flashes, length flashes + 1)
+    pure (sum $ take 100 flashes, length flashes)
