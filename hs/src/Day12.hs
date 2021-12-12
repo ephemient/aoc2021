85a7d17178203e24af2429410dfa42669ad830c9
Daniel Lin <ephemient@gmail.com>
2021-12-12 04:11:35 -0500

    Day 12: Bitset

--- hs/src/Day12.hs
+++ hs/src/Day12.hs
@@ -5,13 +5,13 @@ Description:    <https://adventofcode.com/2021/day/12 Day 12: Passage Pathing>
 {-# LANGUAGE FlexibleContexts, MultiWayIf, OverloadedStrings, TypeApplications #-}
 module Day12 (day12a, day12b) where
 
-import Control.Monad (forM, guard)
+import Control.Monad (forM, guard, when)
 import Control.Monad.Writer (execWriter, tell)
+import Data.Bits (finiteBitSize, setBit, testBit)
 import Data.Char (isUpper)
 import Data.Containers.ListUtils (nubOrd)
 import Data.Functor (($>))
-import Data.Graph.Inductive
-import qualified Data.IntSet as IntSet (empty, insert, notMember)
+import Data.Graph.Inductive (Graph, Gr, lsuc, mkGraph, nodes)
 import qualified Data.Map as Map ((!), (!?), fromList, toList)
 import Data.Monoid (Sum(..))
 import Data.Text (Text)
@@ -35,34 +35,28 @@ parse input = do
             [(a', b', T.all isUpper b), (b', a', T.all isUpper a)]
       )
 
-walk :: (Monad m, Traversable t) => (s -> a -> m (t (s, a))) -> s -> a -> m ()
-walk f = curry walk' where walk' (s, a) = f s a >>= mapM_ walk'
+walk :: (Monad m, Traversable t) => (a -> m (t a)) -> a -> m ()
+walk f = walk' where walk' a = f a >>= mapM_ walk'
+
+day12 :: Bool -> Text -> Maybe Int
+day12 bonus input = do
+    (start, end, g) <- parse @Gr input
+    when (any (>= bonusBit) $ nodes g) $ error "input too large"
+    let step (state, i)
+          | i == end = tell (Sum 1) $> mempty
+          | otherwise = pure $ do
+                (j, big) <- lsuc g i
+                guard $ j /= start
+                if
+                  | big -> pure (state, j)
+                  | not $ testBit state j -> pure (setBit state j, j)
+                  | not $ testBit state bonusBit -> pure (setBit state bonusBit, j)
+                  | otherwise -> mempty
+    pure $ getSum $ execWriter $ walk step (if bonus then 0 else setBit 0 bonusBit :: Int, start)
+  where bonusBit = finiteBitSize (0 :: Int) - 1
 
 day12a :: Text -> Maybe Int
-day12a input = do
-    (start, end, g) <- parse @Gr input
-    let step used src
-          | src == end = tell (Sum 1) $> mempty
-          | otherwise = pure $ do
-                (next, big) <- lsuc g src
-                guard $ next /= start
-                if
-                  | big -> pure (used, next)
-                  | IntSet.notMember next used -> pure (IntSet.insert next used, next)
-                  | otherwise -> mempty
-    pure $ getSum $ execWriter $ walk step IntSet.empty start
+day12a = day12 False
 
 day12b :: Text -> Maybe Int
-day12b input = do
-    (start, end, g) <- parse @Gr input
-    let step s@(used, bonus) src
-          | src == end = tell (Sum 1) $> mempty
-          | otherwise = pure $ do
-                (next, big) <- lsuc g src
-                guard $ next /= start
-                if
-                  | big -> pure (s, next)
-                  | IntSet.notMember next used -> pure ((IntSet.insert next used, bonus), next)
-                  | bonus -> pure ((used, False), next)
-                  | otherwise -> mempty
-    pure $ getSum $ execWriter $ walk step (IntSet.empty, True) start
+day12b = day12 True
