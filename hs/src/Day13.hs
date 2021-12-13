49a34c5cb1a357291d1c2c66be4645efc013810e
Daniel Lin <ephemient@gmail.com>
2021-12-13 13:11:20 -0500

    Day 13: Row-major

--- hs/src/Day13.hs
+++ hs/src/Day13.hs
@@ -6,9 +6,9 @@ Description:    <https://adventofcode.com/2021/day/13 Day 13: Transparent Origam
 module Day13 (day13a, day13b) where
 
 import Control.Arrow ((&&&), (***))
-import Data.Array.Unboxed (UArray, (!), accumArray)
-import Data.Bool (bool)
+import Data.Array.Unboxed (UArray, accumArray, elems)
 import Data.Functor (($>))
+import Data.List.Split (chunksOf)
 import Data.Semigroup (Max(..), Min(..))
 import qualified Data.Set as Set (fromList, size)
 import Data.String (IsString)
@@ -19,14 +19,14 @@ import Text.Megaparsec.Char (newline)
 import Text.Megaparsec.Char.Lexer (decimal)
 
 parsePair :: (Num a, MonadParsec e s m, Token s ~ Char) => m (a, a)
-parsePair = (,) <$> decimal <* single ',' <*> decimal
+parsePair = flip (,) <$> decimal <* single ',' <*> decimal
 
 parseFold :: (Num a, Ord a, MonadParsec e s m, Token s ~ Char, IsString (Tokens s)) => m ((a, a) -> (a, a))
 parseFold = chunk "fold along " *> (single 'x' $> foldX <|> single 'y' $> foldY) <*> (single '=' *> decimal)
 
 foldX, foldY :: (Num a, Ord a) => a -> (a, a) -> (a, a)
-foldX x' (x, y) = (x' - abs (x - x'), y)
-foldY y' (x, y) = (x, y' - abs (y - y'))
+foldX x' (y, x) = (y, x' - abs (x - x'))
+foldY y' (y, x) = (y' - abs (y - y'), x)
 
 day13a :: Text -> Either (ParseErrorBundle Text Void) Int
 day13a input = do
@@ -38,7 +38,7 @@ day13b :: Text -> Either (ParseErrorBundle Text Void) [String]
 day13b input = do
     (points, folds) <- parse parser "" input
     let points' = foldr (flip (.)) id folds <$> points
-        ((Min x0, Min y0), (Max x1, Max y1)) = mconcat $ ((Min *** Min) &&& (Max *** Max)) <$> points'
-        bitmap = accumArray @UArray (||) False ((x0, y0), (x1, y1)) $ (, True) <$> points'
-    pure [[bool '\x2591' '\x2593' $ bitmap ! (x, y) | x <- [x0..x1]] | y <- [y0..y1]]
+        ((Min y0, Min x0), (Max y1, Max x1)) = mconcat $ ((Min *** Min) &&& (Max *** Max)) <$> points'
+        bitmap = accumArray @UArray (const id) '\x2591' ((y0, x0), (y1, x1)) $ (, '\x2593') <$> points'
+    pure $ chunksOf (x1 - x0 + 1) $ elems bitmap
   where parser = (,) <$> parsePair @Int `sepEndBy1` newline <* newline <*> parseFold `sepEndBy` newline <* eof
