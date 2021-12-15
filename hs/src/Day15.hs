2970f8aa3199cd680f7e39d3af8d7bc484737218
Daniel Lin <ephemient@gmail.com>
2021-12-15 04:12:48 -0500

    Day 15: Remove unnecessary (and bad) queue tracking

--- hs/src/Day15.hs
+++ hs/src/Day15.hs
@@ -11,8 +11,6 @@ import Data.Char (digitToInt, isDigit)
 import Data.Functor (($>))
 import Data.Heap (MinPrioHeap)
 import qualified Data.Heap as Heap (insert, singleton, view)
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IntSet (insert, notMember, singleton)
 import Data.List.NonEmpty (NonEmpty((:|)), nonEmpty)
 import Data.Text (Text)
 import qualified Data.Text as T (all, length, lines, unpack)
@@ -25,28 +23,26 @@ day15 :: Int -> Vector Int -> Maybe Int
 day15 width risks = runST $ do
     bests <- MV.replicate (V.length risks) maxBound
     MV.write bests 0 0
-    go bests (Heap.singleton (0, 0)) (IntSet.singleton 0)
+    go bests $ Heap.singleton (0, 0)
   where
-    f :: MVector s Int -> Int -> (MinPrioHeap Int Int, IntSet) -> (Int, Int) -> ST s (MinPrioHeap Int Int, IntSet)
-    f bests c (heap, heapSet) (y, x)
-      | x < 0 || x >= width || y < 0 || y * width + x >= V.length risks = pure (heap, heapSet)
+    f :: MVector s Int -> Int -> MinPrioHeap Int Int -> (Int, Int) -> ST s (MinPrioHeap Int Int)
+    f bests c heap (y, x)
+      | x < 0 || x >= width || y < 0 || y * width + x >= V.length risks = pure heap
       | otherwise = do
         best <- MV.read bests i
-        if risk < best && IntSet.notMember i heapSet
-          then MV.write bests i risk $> (Heap.insert (risk, i) heap, IntSet.insert i heapSet)
-          else pure (heap, heapSet)
+        if risk < best then MV.write bests i risk $> Heap.insert (risk, i) heap else pure heap
       where
         i = y * width + x
         risk = c + risks V.! i
-    go :: MVector s Int -> MinPrioHeap Int Int -> IntSet -> ST s (Maybe Int)
-    go bests (Heap.view -> Just ((_, i), heap)) heapSet
+    go :: MVector s Int -> MinPrioHeap Int Int -> ST s (Maybe Int)
+    go bests (Heap.view -> Just ((_, i), heap))
       | i == V.length risks - 1 = Just <$> MV.read bests i
       | otherwise = do
         c <- MV.read bests i
-        (heap', heapSet') <- foldM (f bests c) (heap, heapSet) $
+        heap' <- foldM (f bests c) heap $
             let (y, x) = i `divMod` width in [(y, x - 1), (y - 1, x), (y + 1, x), (y, x + 1)]
-        go bests heap' heapSet'
-    go _ _ _ = pure Nothing
+        go bests heap'
+    go _ _ = pure Nothing
 
 day15a :: Text -> Maybe Int
 day15a input = do
