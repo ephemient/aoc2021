581b8a9b8e2f38a9adcf6e5cc86a24dc6aa3be96
Daniel Lin <ephemient@gmail.com>
2021-12-21 07:05:52 -0500

    Day 21: BUilt-in caching is faster

--- py/aoc2021/day21.py
+++ py/aoc2021/day21.py
@@ -1,4 +1,4 @@
-from collections import Counter
+import functools
 
 
 def part1(lines):
@@ -18,32 +18,18 @@ def part1(lines):
     return n * score1
 
 
-class Score:
-    def __init__(self):
-        self.data = [None] * 44100
-        self.dice = Counter(
-            i + j + k for i in range(1, 4) for j in range(1, 4) for k in range(1, 4)
-        )
-
-    def __call__(self, player1, player2, score1, score2):
-        i = player1 + 10 * player2 + 100 * score1 + 2100 * score2 - 11
-        if self.data[i]:
-            x, y = self.data[i]
+@functools.lru_cache(maxsize=None)
+def score(player1, player2, score1, score2):
+    x, y = 0, 0
+    for d, n in ((3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)):
+        play = (player1 + d - 1) % 10 + 1
+        if score1 + play < 21:
+            x2, y2 = score(player2, play, score2, score1 + play)
+            x += n * y2
+            y += n * x2
         else:
-            x, y = 0, 0
-            for d, n in self.dice.items():
-                play = (player1 + d - 1) % 10 + 1
-                if score1 + play < 21:
-                    x2, y2 = self(player2, play, score2, score1 + play)
-                    x += n * y2
-                    y += n * x2
-                else:
-                    x += n
-            self.data[i] = x, y
-        return x, y
-
-
-Score = Score()
+            x += n
+    return x, y
 
 
 def part2(lines):
@@ -51,7 +37,7 @@ def part2(lines):
     >>> part2(["Player 1 starting position: 4", "Player 2 starting position: 8"])
     444356092776315
     """
-    x, y = Score(int(lines[0][28:].strip()), int(lines[1][28:].strip()), 0, 0)
+    x, y = score(int(lines[0][28:].strip()), int(lines[1][28:].strip()), 0, 0)
     return max(x, y)
 
 
