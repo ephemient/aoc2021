e20c4802bec9e85c3596a0d580ab620300b93fb2
Daniel Lin <ephemient@gmail.com>
2021-12-31 10:39:39 -0500

    Day 24: No need for special GraalVM input

--- kt/src/jvmMain/kotlin/com/github/ephemient/aoc2021/Resources.kt
+++ kt/src/jvmMain/kotlin/com/github/ephemient/aoc2021/Resources.kt
@@ -1,13 +1,8 @@
 package com.github.ephemient.aoc2021
 
-import org.graalvm.nativeimage.ImageInfo
-
 private enum class Resources
 
-internal actual fun getInput(day: Int): List<String> {
-    val testResource = if (System.getProperty(ImageInfo.PROPERTY_IMAGE_CODE_KEY) == "agent") {
-        Resources::class.java.classLoader.getResourceAsStream("day$day-agent.txt")
-    } else null
-    val resource = testResource ?: Resources::class.java.classLoader.getResourceAsStream("day$day.txt")
-    return checkNotNull(resource) { "No data for day $day" }.bufferedReader().use { it.readLines() }
-}
+actual fun getInput(day: Int): List<String> =
+    checkNotNull(Resources::class.java.classLoader.getResourceAsStream("day$day.txt")) { "No data for day $day" }
+        .bufferedReader()
+        .use { it.readLines() }
